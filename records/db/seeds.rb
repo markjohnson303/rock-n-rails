8369295adcabd6bf7f4b874ae4014332810f4a1a
Mark Johnson <markjohnson303@gmail.com>
2016-07-26 15:06:53 -0600

    seed file added

--- records/db/seeds.rb
+++ records/db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Wipe the database
+Record.destroy_all
+# Let's create a bunch of records
+Record.create([
+  {
+    title: "Transatlantic Psyck Classics Vol 1",
+    artist: "The Electric Peanut Butter Co.",
+    year: 2014,
+    cover_art: "https://f4.bcbits.com/img/a2303719772_10.jpg",
+    song_count: 12
+  },
+  {
+    title: "EMEFE",
+    artist: "EMEFE",
+    year: 2015,
+    cover_art: "https://f4.bcbits.com/img/a1252124777_10.jpg",
+    song_count: 10
+  },
+  {
+    title: "Get Out the Lotion",
+    artist: "Low Cut Connie",
+    year: 2011,
+    cover_art: "https://f4.bcbits.com/img/a2949034103_16.jpg",
+    song_count: 13
+  }
+])
