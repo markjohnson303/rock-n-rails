78b193e246b7d18f6ee97de0a67a164b70a290fa
Mark Johnson <markjohnson303@gmail.com>
2016-07-31 09:17:19 -0600

    complete

--- records/app/controllers/records_controller.rb
+++ records/app/controllers/records_controller.rb
@@ -15,4 +15,17 @@ class RecordsController < ApplicationController
 		render :new
 	end
 
+	def create
+	  # creating a record in the database
+	  Record.create(record_params)
+	  redirect_to('/records')
+	end
+
+	private
+
+	# strong parameters for a record
+	def record_params
+	  params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+	end
+
 end
